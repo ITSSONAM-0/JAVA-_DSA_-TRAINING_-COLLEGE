c73f8694c97c94c691e8cc2f8727e700f95441eb
sonam kumari <135491610+ITSSONAM-0@users.noreply.github.com>
2025-11-10 13:55:31 +0530

    Update b.java

--- b.java
+++ b.java
@@ -5,7 +5,7 @@ public class b {
         int start = 0, end = numbers.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
-            // comparisons
+          
             if (numbers[mid] == key) {
                 return mid;
             }
